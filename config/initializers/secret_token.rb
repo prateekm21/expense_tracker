5c474a8097921e450ac0c85a71ffb28284eab327
Prateek Mehrotra <prateek.mehrotra@spirent.com>
2013-03-28 16:18:15 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExpenseTracker::Application.config.secret_token = '9c1acbd58c375849284aed89b0bff3c3602be1d5b0afef54e9d7acced22bdda87931e175fd39dfb9227d5f107b32be9eac3d3bd5dcfb69b30b42939a6de93b8d'
